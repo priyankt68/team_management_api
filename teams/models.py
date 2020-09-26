183bff17be892231905ff93ec57179ecf1c26876
Priyank Trivedi <priyanktrivedi@Arpits-MacBook-Pro-2.local>
2020-09-26 15:09:58 +0530

    adding model for team Members and registering app in root settings

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    role = models.CharField(choices=[("admin", "Admin"), ("regular", "Regular")], max_length=10)
+
+    @property
+    def fullname(self):
+        return self.first_name + self.last_name
+    
+    def __unicode__(self):
+        return self.first_name + self.last_name
+    
+
--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("index url")
+
+
+
+class Members(View):
+    def get(self, request):
+        
+    
+        return HttpResponse('result')
