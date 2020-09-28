d3a1133bd13092d79fe68936e193f12468796c82
Priyank Trivedi <priyanktrivedi@Arpits-MacBook-Pro-2.local>
2020-09-28 16:31:03 +0530

    adding jsonised response

--- teams/views.py
+++ teams/views.py
@@ -9,18 +9,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+import json
+
 class MemberList(APIView):
 
     def get(self, request, format=None):
         members = Member.objects.all()
         serializer = MemberSerializer(members, many=True)
-        return HttpResponse(serializer.data)
+        return HttpResponse(json.dumps(serializer.data))
 
     def post(self, request, format=None):
         serializer = MemberSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return HttpResponse(serializer.data, status=status.HTTP_201_CREATED)
+            return HttpResponse(json.dumps(serializer.data), status=status.HTTP_201_CREATED)
         return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class MemberDetail(APIView):
@@ -34,14 +36,14 @@ class MemberDetail(APIView):
     def get(self, request, pk, format=None):
         memberobject = self.get_object(pk)
         serializers = MemberSerializer(memberobject)
-        return HttpResponse(serializers.data)
+        return HttpResponse(json.dumps(serializers.data))
     
     def put(self, request, pk, format=None):
         memberobject = self.get_object(pk)
         serializer = MemberSerializer(memberobject, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return HttpResponse(serializer.data)
+            return HttpResponse(json.dumps(serializer.data))
         return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
